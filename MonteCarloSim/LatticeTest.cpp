ecefcfb7f0decf825fd955731ecf88dedd9a2b92
mlazarn <micarn@vt.edu>
2018-06-07 17:03:08 -0400

    temp

--- MonteCarloSim/LatticeTest.cpp
+++ MonteCarloSim/LatticeTest.cpp
@@ -5,13 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-<<<<<<< HEAD
-    std::string path = "firstLargeRun/";
-
-    cout << "Initializing lattice: " << endl;
-=======
     string path = "data/thirdMonteCarloRun/";
->>>>>>> 94a8a10cc8cd8bdfb10b271398051800ebc5b7ca
 
     Lattice testLattice(path, 256);
     
@@ -22,11 +16,7 @@ int main(int argc, char *argv[])
     testLattice.dataOutput();
 
     //testLattice.reactTest();
-<<<<<<< HEAD
-    //testLattice.monteCarloRun(1000000000, 1000000, 50000000);
-=======
     testLattice.monteCarloRun(800000000, 1000000, 50000000);
->>>>>>> 94a8a10cc8cd8bdfb10b271398051800ebc5b7ca
 
     cout << "test complete" << endl;
     return 0;
