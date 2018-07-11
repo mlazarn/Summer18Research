599e48bd7e6945b61a5e7f006911d8477b15b55e
mlazarn <micarn@vt.edu>
2018-07-11 14:59:46 -0400

    modified mlrps_run.sh

--- MonteCarloSim/mlrps_run.sh
+++ MonteCarloSim/mlrps_run.sh
@@ -42,20 +42,20 @@
     #done
 #done
 
-target="data/animTest"
-prefix="latt_"
-density_pfx="density_"
-flux_pfx="flux_"
-xSize="256"
-ySize="512"
-intDist="64"
-mobility="5.0"
-RPSMobility="0.1"
-steps="2000"
-interval="5"
-start_t="1000"
-author="micarn"
-species=('a' 'b' 'c')
+#target="data/animTest"
+#prefix="latt_"
+#density_pfx="density_"
+#flux_pfx="flux_"
+#xSize="256"
+#ySize="512"
+#intDist="64"
+#mobility="5.0"
+#RPSMobility="0.1"
+#steps="2000"
+#interval="5"
+#start_t="1000"
+#author="micarn"
+#species=('a' 'b' 'c')
 
 #mkdir -p -v $target
 #./LatticeMLRPSTest $target 0 1 $xSize $ySize $mobility $RPSMobility $intDist $steps $interval $start_t
@@ -63,10 +63,10 @@ species=('a' 'b' 'c')
 #python3 videoConverter.py $target $prefix $start_t $interval $steps -o animation.mp4 -a $author
 
 #flux A
-python3 densityCalculator.py a $target flux_lims_test_a.mp4 f 0 $ySize -c 0 -p $flux_pfx -v 64 -s $start_t -i $interval -S $steps -a $author
+#python3 densityCalculator.py a $target flux_lims_test_a.mp4 f 0 $ySize -c 0 -p $flux_pfx -v 64 -s $start_t -i $interval -S $steps -a $author
 
 #flux net
-python3 densityCalculator.py a $target flux_lims_test_net.mp4 f 0 $ySize -c -1 -p $flux_pfx -l -0.05 0.05 -v 32 64 96 -s $start_t -i $interval -S $steps -a $author
+#python3 densityCalculator.py a $target flux_lims_test_net.mp4 f 0 $ySize -c -1 -p $flux_pfx -l -0.05 0.05 -v 32 64 96 -s $start_t -i $interval -S $steps -a $author
 
 #density A
 #python3 densityCalculator.py a $target density_a.mp4 p 0 $ySize -c 0 -p $density_pfx -s $start_t -i $interval -S $steps -a $author
@@ -74,20 +74,57 @@ python3 densityCalculator.py a $target flux_lims_test_net.mp4 f 0 $ySize -c -1 -
 #density net
 #python3 densityCalculator.py a $target density_net.mp4 p 0 $ySize -c -1 -p $density_pfx -s $start_t -i $interval -S $steps -a $author
 
-#target="data/currentTest/Testx"
-#prefix="latt_"
-#curr_prefix="curr_"
-#xSize="256"
-#ySize="512"
-#intDist="64"
-#mobility="5.0"
-#RPSMobilities=('0.1' '1.0' '5.0' '10.0') 
-#steps="5000"
-#interval="5"
-#start_t="5"
-#author="micarn"
-#species=('a' 'b' 'c')
-#
+base="data/currentTest2/mob_x/"
+targ="rps_mob_y"
+prefix="latt_"
+density_pfx="density_"
+flux_pfx="flux_"
+lims="-0.05 0.05"
+vlines="64 96 128 160 192"
+xSize="512"
+ySize="768"
+ylims="0 $ySize"
+intDist="128"
+mobilities=('1.25' '2.5' '5.0')
+RPSMobilities=('0.1' '1.0' '2.5' '5.0' '10.0') 
+steps="5000"
+interval="5"
+start_t="5"
+fps="120"
+author="micarn"
+species=('a' 'b' 'c')
+
+for mobility in "${mobilities[@]}"; do
+    for rps_mobility in "${RPSMobilities[@]}"; do
+        mob="${mobility/./_}"
+        rps_mob="${rps_mobility/./_}"
+        target="${base/x/$mob}${targ/y/$rps_mob}"
+
+        mkdir -p -v $target
+
+        #                  targ    o t xSize  ySize  mob       rps_mob      intDist  steps  interval  start_t
+        #                  1       2 3 4      5      6         7            8        9      10        11
+        ./LatticeMLRPSTest $target 0 1 $xSize $ySize $mobility $RPSMobility $intDist $steps $interval $start_t
+
+        python3 videoConverter.py $target $prefix $start_t $interval $steps -o animation.mp4 -a $author
+
+        #flux A
+        python3 densityCalculator.py a $target flux_lims_test_a.mp4 f 0 $ySize -c 0 -p $flux_pfx -v $vlines -s $start_t -i $interval -S $steps -a $author
+
+        #flux net
+        python3 densityCalculator.py a $target flux_lims_test_net.mp4 f 0 $ySize -c -1 -p $flux_pfx -l $lims -v $vlines -s $start_t -i $interval -S $steps -a $author
+
+        #density A
+        python3 densityCalculator.py a $target density_a.mp4 p 0 $ySize -c 0 -p $density_pfx -v $vlines -s $start_t -i $interval -S $steps -a $author
+
+        #density net
+        python3 densityCalculator.py a $target density_net.mp4 p 0 $ySize -c -1 -p $density_pfx -v $vlines -s $start_t -i $interval -S $steps -a $author
+
+    done
+done
+
+cd data
+tar -zcvf currentTest2Renders.tar.gz currentTest2/*/*/*.mp4 currentTest2/*/*/metadata.txt
 #for n in {0..3}; do
 #    targ=${target/%x/$n}
 #    ./LatticeMLRPSTest $targ 0 1 $xSize $ySize $mobility ${RPSMobilities[$n]} $intDist $steps $interval $start_t
@@ -98,6 +135,7 @@ python3 densityCalculator.py a $target flux_lims_test_net.mp4 f 0 $ySize -c -1 -
 #        destination="--dest_movie density_${species[$s]}.mp4"
 #        curr_destination="--dest_movie current_${species[$s]}.mp4"
 #        python3 densityCalculator.py m $targ $intDist $destination -c $s -t 1 -r $xSize -p $prefix -s $start_t -i $interval -S $steps -a $author
+
 #        python3 densityCalculator.py cm $targ $intDist $curr_destination -c $s -t 1 -r $xSize -p $curr_prefix -s $start_t -i $interval -S $steps -a $author
 #    done
 #done
