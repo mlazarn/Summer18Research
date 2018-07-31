c746d1f332c033e53ea62c8952313102812b1d1a
mlazarn <micarn@vt.edu>
2018-07-31 16:28:36 -0400

    implemented graphing of spectral analysis data

--- MonteCarloSim/fourierAnalysis.py
+++ MonteCarloSim/fourierAnalysis.py
@@ -1,25 +1,50 @@
 import os
 import sys
+import argparse as ap
 
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 import matplotlib.animation as manimation
+from matplotlib import colors as colors
 
 from tqdm import tqdm
 
-def generate_array(prefix, timesteps, width):
-    array = np.zeros(timesteps + 1, width)
-    for t in range(timesteps + 1):
-        filename = prefix + str(t) + ".csv"
-        array[i] = np.genfromtxt(filename, dtype=float, delimiter=',')[0, :]
-    return array
+parser = ap.ArgumentParser()
+parser.add_argument('target')
+parser.add_argument('prefix')
+parser.add_argument('runs', type=int)
+parser.add_argument('--dpi', type=int, default=100)
 
-def take_fft(array, timesteps, width):
-    array_transpose = a.T
-    dims = array_transpose.shape()
-    fft_array = np.zeros(dims)
-    
-    for x in range(width):
+args = parser.parse_args()
 
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 
+os.chdir(args.target)
+print(os.getcwd())
+
+plotSpectrograph(args):
+
+def plotSpectrograph(args):
+    targ = args.prefix + '0';
+    stacked = np.genfromtxt(targ + '/spectralData.csv', dtype=float, delimiter=',')[:,1:50].T[::-1,:]
+
+    for run in range(1, args.runs):
+        targ = args.prefix + str(run);
+        new_array = np.genfromtxt(targ + '/spectralData.csv', dtype=float, delimiter=',')[:1:50:-1].T[::-1,:]
+        stacked = np.dstack((stacked, new_array))
+
+    spec_data = np.mean(stacked, axis=2)
+
+    fig, ax = plt.subplots()
+    con = ax.imshow(spec_data, cmap='inferno')
+    cb = fig.colorbar(con, ax=ax)
+    ax.set_aspect(5)
+
+    fig.set_tight_layout(True)
+
+    ax.set_xlabel(r'$\omega$')
+    ax.set_ylabel(r'$r$')
+
+    fig.savefig("spacData.png", dpi=200)
