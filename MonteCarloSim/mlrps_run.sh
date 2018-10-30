85fcd3a2a3fb5499fd37e7962557e89a85b99482
mlazarn <micarn@vt.edu>
2018-10-30 10:39:26 -0400

    minor

--- MonteCarloSim/mlrps_run.sh
+++ MonteCarloSim/mlrps_run.sh
@@ -113,19 +113,19 @@ normOut="normSpecData.png"
 
 #for n in {0..0}; do
     targ="${base}"
-    for m in {0..14}; do
-        target="$targ/${dir_suffix}${m}"
-        mkdir -p -v $target
+    #for m in {0..14}; do
+        #target="$targ/${dir_suffix}${m}"
+        #mkdir -p -v $target
         #                  targ    o t xSize  ySize  mob       rps_mob      intDist  bin_w     steps  interval         start_t  run
         #                  1       2 3 4      5      6         7            8        9         10     11               12       13
-        ./LatticeMLRPSTest $target 0 1 $xSize $ySize $mobility $RPSMobility $intDist $binWidth $steps $interval $start_t $m
-    done
+        #./LatticeMLRPSTest $target 0 1 $xSize $ySize $mobility $RPSMobility $intDist $binWidth $steps $interval $start_t $m
+    #done
     #python3 fourierAnalysis.py $targ temporalData.csv $output $dir_suffix 100 15 s ${pad[$n]} -v $vlines --dpi $dpi -a 10.0
     #python3 fourierAnalysis.py $targ temporalData.csv $normOut $dir_suffix 100 15 s ${pad[$n]} -v $vlines --dpi $dpi -a 10.0 --abs
     python3 fourierAnalysis.py $targ temporalData.csv $normOut $dir_suffix 100 15 s ${pad[2]} -v $vlines --dpi $dpi -a 10.0 --abs -o -w -d "spectrograph.csv"
     python3 fourierAnalysis.py $targ temporalData.csv $normOut $dir_suffix 100 15 s ${pad[2]} -v $vlines --dpi $dpi -a 10.0 --abs -o -w -d "hwhm.csv"
     #python3 fourierAnalysis.py $targ temporalData.csv freqAnalysisPadded.png $dir_suffix 100 50 f ${pad[2]} -p 0 --dpi $dpi --abs -o
-    python3 videoConverter.py ${targ}/${dir_suffix}0 $prefix $start_t $interval $steps -v $vlines -o animation.mp4 -a $author -f $fps --dpi $dpi
+    #python3 videoConverter.py ${targ}/${dir_suffix}0 $prefix $start_t $interval $steps -v $vlines -o animation.mp4 -a $author -f $fps --dpi $dpi
 #done
 
 #for l in {0..3}; do
