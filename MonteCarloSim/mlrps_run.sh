b39006f9f8b48c0ba9e9e641fea4dfef63f88815
mlazarn <micarn@vt.edu>
2018-10-30 10:03:05 -0400

    minor

--- MonteCarloSim/mlrps_run.sh
+++ MonteCarloSim/mlrps_run.sh
@@ -118,7 +118,7 @@ for n in {0..0}; do
         mkdir -p -v $target
         #                  targ    o t xSize  ySize  mob       rps_mob      intDist  bin_w     steps  interval         start_t  run
         #                  1       2 3 4      5      6         7            8        9         10     11               12       13
-        ./LatticeMLRPSTest $target 0 1 $xSize $ySize ${mobilities[$n]} $RPSMobility $intDist $binWidth $steps $interval $start_t $m
+        ./LatticeMLRPSTest $target 0 1 $xSize $ySize $mobility $RPSMobility $intDist $binWidth $steps $interval $start_t $m
     done
     #python3 fourierAnalysis.py $targ temporalData.csv $output $dir_suffix 100 15 s ${pad[$n]} -v $vlines --dpi $dpi -a 10.0
     #python3 fourierAnalysis.py $targ temporalData.csv $normOut $dir_suffix 100 15 s ${pad[$n]} -v $vlines --dpi $dpi -a 10.0 --abs
